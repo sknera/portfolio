90dc533b11fd6570f38da5720a5ee4152ba449ac
Adrian Charkiewicz <riraasaa@gmail.com>
2017-06-13 12:24:00 +0200

    Add files via upload

---
+++ scrollTo.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(e) {
+    
+	$('.element').click(function(){
+		
+		var scrollTo = $(this).attr('data-scrollTo');
+		
+
+		$('body,html').animate({
+			"scrollTop":$('#'+scrollTo).offset().top}, 1000);
+		return false;
+
+		});
+});
+
